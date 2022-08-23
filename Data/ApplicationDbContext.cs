b953cb9af3fb72d06cf9482450f995b1ba283a66
LENOVO/HI <praveenraj.v@blackandwhiteconsulting.com>
2022-08-23 16:21:52 +0530

    Initial comit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RewardSystem.Models;
+
+namespace RewardSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerTransaction> CustomerTransactions { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //{
+        //    options.UseSqlite(@"Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\CustomerRewardSystem.db");
+        //    //options.UseSqlite(@"Data Source=F:\Code-Practice\Synergent\CustomerRewardSystem\CustomerRewardSystem.db");
+        //}
+    }
+}
---
+++ Models/CustomerTransaction.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+
+namespace RewardSystem.Models
+{
+    [Index(nameof(TransactionCode), IsUnique = true)]
+    public class CustomerTransaction
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public string TransactionCode { get; set; }
+
+        [Required]
+
+        public DateTime TransactionDatetime { get; set; }
+        
+        [Required]
+        public int TransactionAmount { get; set; }
+        public int TransactionReward { get; set; }
+
+    }
+}
